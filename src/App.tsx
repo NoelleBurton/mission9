f54fcdba20165c957f32fd767bbe79fd94e28d65
Noelle Burton <wellyyburton@gmail.com>
2024-03-08 20:42:02 -0700

    Cards done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './data.json';
 
+// create an interface
+interface Team {
+  tid: number;
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+// root object has a key called "teams"
+const teams: Team[] = data.teams;
+
+// #1 welcome function
+function Welcome() {
+  return (
+    <div>
+      <h1>March Madness</h1>
+      <h3>
+        This app displays the school name, mascot name, and location for all the
+        basketball teams in the NCAA.
+      </h3>
+    </div>
+  );
+}
+
+// #2 component to create an individual card
+class TeamComponent extends React.Component<{ team: Team }, {}> {
+  render() {
+    const { team } = this.props;
+    return (
+      <div className="team-card">
+        <h4>{team.school}</h4>
+        <p>Macot Name: {team.name}</p>
+        <p>
+          Location: {team.city}, {team.state}
+        </p>
+      </div>
+    );
+  }
+}
+
+// #3 class to create all cards
+function TeamList() {
+  return (
+    <div>
+      {teams.map((team) => (
+        <TeamComponent key={team.tid} team={team} />
+      ))}
+    </div>
+  );
+}
+
+// app function
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Testing
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
